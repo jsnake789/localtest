bd6fc0b0de7941e9b8a0909fd2bbc5b9cd79d65f
Jake Ralston <jsnake789@gmail.com>
2021-08-19 09:00:13 -0500

    First Commit

---
+++ evariables.cmd
@@ -0,0 +1,7 @@
+set AZURE_SUBSCRIPTION_ID=f4c81b7b-69e0-4649-a892-7bc659153e3a
+set AZURE_TENANT_ID=704aa8f8-ba9c-4d12-ad72-ab69c06d136c
+set AZURE_CLIENT_ID=a1caf60b-a004-4ff6-bde5-5bb9d505c563
+set AZURE_CLIENT_SECRET=12-Uf_~XobyRi~.INstPZC1MX~pj4bTzq8
+set STORAGE_BLOB_URL=https://pythonazurestorage05606.blob.core.windows.net
+set AZURE_STORAGE_CONNECTION_STRING=DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=pythonazurestorage05606;AccountKey=wocpygYpvnYq1w2m3nXthO1MbCGVURnAWHCsoahIHya/i4ep3ALu6sgph7abkZog8+JWYmW26yrEs9cjt0WPpQ==
+set REPO_URL=https://github.com/jsnake789/storage-blob-upload-from-webapp
